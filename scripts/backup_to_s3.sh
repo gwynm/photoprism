a3e433f32eb7d5527d204b3573b878581891e501
Gwyn Morfey <mail@gwynmorfey.com>
2021-05-29 07:38:26 +0100

    Glacier

--- scripts/backup_to_s3.sh
+++ scripts/backup_to_s3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir /photoprism/db_backups
 photoprism backup -i -a - | gzip > /photoprism/db_backups/backup_`date "+%Y-%m-%d-%H-%M-%S"`.gz
-rclone sync --s3-provider="AWS" --s3-env-auth="true" --s3-region=$AWS_REGION /photoprism/db_backups :s3:$AWS_BUCKET/db_backups
-rclone sync --s3-provider="AWS" --s3-env-auth="true" --s3-region=$AWS_REGION /photoprism/originals :s3:$AWS_BUCKET/originals
+rclone sync --s3-provider="AWS" --s3-env-auth="true" --s3-region=$AWS_REGION --s3-storage-class="GLACIER" /photoprism/db_backups :s3:$AWS_BUCKET/db_backups
+rclone sync --s3-provider="AWS" --s3-env-auth="true" --s3-region=$AWS_REGION --s3-storage-class="GLACIER" /photoprism/originals :s3:$AWS_BUCKET/originals
