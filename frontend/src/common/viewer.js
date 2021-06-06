772bfe5f46ec42e8e772b4c5e591f75700d6f13c
Gwyn Morfey <mail@gwynmorfey.com>
2021-06-06 09:08:22 +0100

    Finish relocating play button

--- frontend/src/common/viewer.js
+++ frontend/src/common/viewer.js
@@ -131,11 +131,6 @@ class Viewer {
 
         captionEl.children[0].innerHTML = Util.encodeHTML(item.description);
 
-        if (item.playable) {
-          captionEl.children[0].innerHTML +=
-            ' <i aria-hidden="true" class="v-icon material-icons theme--dark" title="Play">play_circle_fill</i>';
-        }
-
         // if (item.description) {
         //   captionEl.children[0].innerHTML +=
         //     '<br><span class="description">' + Util.encodeHTML(item.description) + "</span>";
